a3bdc558547bb6323f0a679006182e88f87eccea
RamitSharma991 <RamitsMac@Ramits-MBP.fritz.box>
2019-01-16 13:35:47 +0530

    initial commit

---
+++ Treads/Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by Ramit sharma on 16/01/19.
+//  Copyright © 2019 Ramit sharma. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Treads/Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Ramit sharma on 16/01/19.
+//  Copyright © 2019 Ramit sharma. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
